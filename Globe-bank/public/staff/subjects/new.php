c6695e60a5cc9f1349b4f4b8af280ba5038c10e6
nimal krishna <nimalwyd@gmail.com>
2017-10-01 16:05:46 -0400

    test

---
+++ Globe-bank/public/staff/subjects/new.php
@@ -0,0 +1,58 @@
+<?php require_once('../../../private/initialize.php'); 
+
+$subject_set=find_all_subjects();
+$subject_count=mysqli_num_rows($subject_set)+1;
+mysqli_free_result($subject_set);
+
+$subject=[];
+$subject["position"]=$subject_count;
+?>
+<?php
+$page_title ='Create Subject';
+include(SHARED_PATH.'/staff_header.php');
+
+?>
+
+<div id="content">
+	<a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">
+&laquo; Back to list </a>
+<div class="subject new">
+<h1>Create subject</h1>
+
+
+<form action="<?php echo url_for('/staff/subjects/create.php');   ?>" method="post">
+<dl>  
+<dt>menu Name</dt>
+<dd><input type="text" name="menu_name" value="" /></dd>
+</dl>
+<dl>
+<dt> Position</dt>
+<dd>
+<select name="postion">
+<?php
+  for($i=1; $i <= $subject_count; $i++) {
+    echo "<option value=\"{$i}\"";
+    if($subject["position"] == $i) {
+      echo " selected";
+    }
+    echo ">{$i}</option>";
+  }
+?>
+</select>
+</dd>
+</dl>
+<dl>
+<dt>Visible</dt>
+<dd>
+<input type="hidden" name="visible" value="0" />
+<input type="checkbox" name="visible" value="1" />
+</dd>
+</dl>
+<div id="operation">
+<input type="submit"value="create subject"/>
+</div>
+</form>
+</div>
+</div>
+
+
---
+++ globe_bank_final/private/db_credentials.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_SERVER", "localhost");
+define("DB_USER", "user1");
+define("DB_PASS", "n");
+define("DB_NAME", "globe_bank");
+
+?>
